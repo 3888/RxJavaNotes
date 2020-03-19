49d9f7100ce2ccd264cec590786afc6ac36bd6f0
Anton <krrg@mail.ru>
2020-03-19 23:59:16 +0200

    added Rx

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,15 @@
-fun main(){
-    println("Hi")
+import io.reactivex.rxjava3.core.Flowable
+import io.reactivex.rxjava3.core.Observable
+
+fun main() {
+
+    Flowable.just("Hello world").subscribe(System.out::println)
+
+    Observable.just(2, 30, 22, 5, 60, 1)
+        .filter { x -> x > 10}
+        .subscribe {
+         println("item: $it" )
+        }
+
+
 }
