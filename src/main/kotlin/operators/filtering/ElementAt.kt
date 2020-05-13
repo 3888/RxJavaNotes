9af915065583a42ba5e6c7ea46753280b49a2b52
Anton <krrg@mail.ru>
2020-05-13 20:10:14 +0300

    added ignoreElements, elementAt

---
+++ src/main/kotlin/operators/filtering/ElementAt.kt
@@ -0,0 +1,16 @@
+package operators.filtering
+
+import io.reactivex.Observable
+
+fun main() {
+    elementAt()
+}
+
+private fun elementAt() {
+    Observable.just(1, 2, 3, 4, 5)
+        .elementAt(1)
+//        .elementAt(-1) // IndexOutOfBoundsException: index >= 0 required but it was -1
+        .subscribe {
+            println(it)
+        }
+}
---
+++ src/main/kotlin/operators/filtering/IgnoreElements.kt
@@ -0,0 +1,36 @@
+package operators.filtering
+
+import io.reactivex.Observable
+import io.reactivex.Single
+import java.util.concurrent.atomic.AtomicInteger
+
+
+fun main() {
+    ignoreElements()
+}
+
+private fun ignoreElements() {
+/*
+Есть операторы, возвращающие Completable. Так что если вас не интересуют данные, и вы просто ждёте завершения или ошибки,
+то можно использовать оператор ignoreElements.
+ */
+
+    /*
+    https://mindorks.com/android/store/Reactive-Programming/reactivex/rxjava
+    */
+    val count = AtomicInteger()
+
+    Observable.
+    range(1, 10)
+        .doOnNext { count.incrementAndGet() }
+        .ignoreElements()
+        .andThen(Single.fromCallable { count.get() })
+        .subscribe({
+            println(it)
+        }, {
+            println("Error $it")
+        })
+}
+
+
+
