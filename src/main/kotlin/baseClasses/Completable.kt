78c9daf145a26c9143908a8b2410356e042ec5e1
Anton <krrg@mail.ru>
2020-05-27 15:33:35 +0300

    added andThen() vs andThen{}

--- src/main/kotlin/baseClasses/Completable.kt
+++ src/main/kotlin/baseClasses/Completable.kt
@@ -4,7 +4,19 @@ import io.reactivex.Completable
 
 fun main() {
 
-    completable()
+    doOnComplete()
+//        .andThen(doOnError())
+//        .andThen { doOnError() }
+        .andThen(doOnComplete())
+//        .andThen {
+//            doOnComplete()
+//            .subscribe({
+//                println("All done now HERE!")
+//            },
+//                {
+//                    println(it)
+//                })
+//        }
         .subscribe({
             println("All done")
         },
@@ -12,13 +24,36 @@ fun main() {
                 println(it)
             })
 
+//    doOnError()
+//        .andThen(doOnComplete())
+////        .andThen { doOnComplete() }
+//        //        .andThen(doOnError())
+////        .andThen { doOnError() }
+//        .subscribe({
+//            println("All done")
+//        },
+//            {
+//                println(it)
+//            })
+
 }
 
-private fun completable(): Completable {
-    return Completable.create { subscriber ->
-        for (i in listOf(2, 4, 0)) {
-            println("4 / $i = ${4 / i}")
+private fun doOnError(): Completable {
+    return Completable
+        .create { subscriber ->
+            for (i in listOf(2, 4, 0)) {
+                println("4 / $i = ${4 / i}")
+            }
+            subscriber.onComplete()
         }
-        subscriber.onComplete()
-    }
+}
+
+private fun doOnComplete(): Completable {
+    return Completable
+        .create { subscriber ->
+            for (i in listOf(2, 4, 1)) {
+                println("4 / $i = ${4 / i}")
+            }
+            subscriber.onComplete()
+        }
 }
